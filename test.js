b8cb4d887973cf1b55f1b06ad7599f3e978136a2
Peter Ajtai <peter.ajtai@gmail.com>
2012-11-08 16:00:37 -0800

    added a quick test and ability for mixins

---
+++ test.js
@@ -0,0 +1,34 @@
+
+var playerFacade = {
+    play: 'method to play movies',
+    stop: 'method to stop movies'
+};
+
+var remoteFacade = {
+    on: 'turn on',
+    off: 'turn off'
+};
+
+// An implelemntation of the interface
+var htmlPlayer = {
+    play: function() { console.log("html play"); this.helper(); return this; },
+    stop: function() { console.log("html stop"); return this; },
+    helper: function() { console.log("player helper fired"); }
+};
+
+var remote = {
+    on: function() { this.helper(); console.log("remote on"); return this; },
+    off: function() { console.log("remote off"); return this; }
+};
+
+
+// -----------------------------------------
+
+// To implelemnt an interface:
+var player = Facade(playerFacade, htmlPlayer).mixIn(remoteFacade, remote);
+
+
+player.on().play().stop().off();
+
+// Helper is not directly accessible: (uncomment to check)
+// htmlInterface.helper();
