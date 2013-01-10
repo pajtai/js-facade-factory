a6c81c560f4e1ecaadbbcdc3eaba817073e546ca
Peter Ajtai <peter.ajtai@thinksolid.com>
2013-01-10 13:23:23 -0800

    adding builds and tests

---
+++ tests/sample/test.js
@@ -0,0 +1,36 @@
+
+var playerFacade = {
+    play: 'method to play movies',
+    stop: 'method to stop movies'
+};
+
+var remoteFacade = {
+    on: 'turn on',
+    off: 'turn off'
+};
+
+// An implelemntation of the interface
+var htmlPlayer = {
+    play: function() { console.log("html play"); this.helper(this.helperProp); return this; },
+    stop: function() { console.log("html stop"); return this; },
+    helper: function(prop) { console.log("player helper fired"); console.log('player helper property passed in: ' + prop); console.log('player helper prop defined: ' + this.helperProp) },
+    helperProp: true
+};
+
+var remote = {
+    on: function() { this.helper(this.helperProp); console.log("remote on"); return this; },
+    off: function() { console.log("remote off"); return this; }
+};
+
+
+// -----------------------------------------
+
+// To implelemnt an interface:
+var player = Facade(playerFacade, htmlPlayer).mixIn(remoteFacade, remote);
+
+
+player.on().play().stop().off();
+
+// Helper is not directly accessible: (uncomment to check)
+console.log(typeof player.helper);
+console.log(typeof player.helperProp);
